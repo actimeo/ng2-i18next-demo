3e47a7b495f071c917b8c1313ad158f37a966cd3
pmartin <contact@elol.fr>
2016-04-17 14:18:41 +0200

    Use I18nDirective

--- src/client/app/ng2-i18next-demo.ts
+++ src/client/app/ng2-i18next-demo.ts
@@ -1,11 +1,12 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {I18nDirective} from 'ng2-i18next/ng2-i18next';
 
 @Component({
   selector: 'ng2-i18next-demo-app',
   providers: [ROUTER_PROVIDERS],
   templateUrl: 'app/ng2-i18next-demo.html',
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, I18nDirective],
   pipes: []
 })
 @RouteConfig([
