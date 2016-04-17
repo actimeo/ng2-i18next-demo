140e11afc0664f466d80c1d0ee29a64aa47de775
pmartin <contact@elol.fr>
2016-04-17 14:07:52 +0200

    Add i18next packages to list npm vendor packages

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -4,7 +4,12 @@ var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
 
 module.exports = function(defaults) {
   var app = new Angular2App(defaults, {
-    vendorNpmFiles: []
+    vendorNpmFiles: [
+	'i18next/**', 
+        'i18next-xhr-backend/**', 
+        'i18next-browser-languagedetector/**', 
+        'ng2-i18next/**'
+    ]
   });
   return app.toTree();
 };
