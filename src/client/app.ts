d469677e28f72343b01756cbf236d81ea224ad79
pmartin <contact@elol.fr>
2016-04-17 14:11:51 +0200

    Inject I18nService at bootstrap time

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,4 +1,5 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {Ng2I18nextDemoApp} from './app/ng2-i18next-demo';
+import {I18nService} from 'ng2-i18next/ng2-i18next';
 
-bootstrap(Ng2I18nextDemoApp, []);
+bootstrap(Ng2I18nextDemoApp, [I18nService]);
